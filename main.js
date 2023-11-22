fdd4031faa37386cf76f85ecf6f1c66b7f076ae6
Vinicius Melo <ferraz.viniciusmelo@estudante.ufjf.br>
2023-11-21 22:14:59 -0300

    Seleciona elementos

---
+++ main.js
@@ -0,0 +1,9 @@
+const pEntrada = document.querySelector("#entrada");
+const btnAdicionar = document.querySelector("#adicionar");
+const btnLimpar = document.querySelector("#limpar");
+const olnItens = document.querySelector("#itens");
+
+const li = document.createElement("li");
+li.textContent = "Primeiro";
+console.log(li);
+olnItens.appendChild(li);

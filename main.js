0ac2b839b9b4429e1eeeb755c9f9552a8c5df223
Vinicius Melo <ferraz.viniciusmelo@estudante.ufjf.br>
2023-11-21 22:51:27 -0300

    Adiciona atualizar lista

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+import { getLista } from "./src/lista.js";
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olnItens = document.querySelector("#itens");
 
-adicionaElementoNaLista("Primeiro");
-adicionaElementoNaLista("Segundo");
-adicionaElementoNaLista("Terceiro");
+atualizarListaOrdenada();
 
-function adicionaElementoNaLista(texto){
+function atualizarListaOrdenada(){
+    const Lista = getLista();
+    for(let i=0; i<Lista.length; i++){
+        adicionaElementoNaListaOrdenada(Lista[i]);
+    }
+
+}
+
+
+function adicionaElementoNaListaOrdenada(texto){
     const li = document.createElement("li");
     li.textContent = texto;
     olnItens.appendChild(li);

28657081d6ed94d9375ef7bbe84765dfb906a7a6
Vinicius Melo <ferraz.viniciusmelo@estudante.ufjf.br>
2023-11-21 22:59:40 -0300

    Adiciona item no estado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getLista } from "./src/lista.js";
+import { adicionaNaLista, getLista } from "./src/lista.js";
 
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
@@ -6,9 +6,17 @@ const btnLimpar = document.querySelector("#limpar");
 const olnItens = document.querySelector("#itens");
 
 atualizarListaOrdenada();
+btnAdicionar.addEventListener('click', adicionaItemEntrada);
+function adicionaItemEntrada(){
+    const valor = pEntrada.textContent;
+    adicionaNaLista(valor);
+    pEntrada.textContent = "";
+    atualizarListaOrdenada();
+}
 
 function atualizarListaOrdenada(){
     const Lista = getLista();
+    olnItens.innerHTML = "";
     for(let i=0; i<Lista.length; i++){
         adicionaElementoNaListaOrdenada(Lista[i]);
     }
